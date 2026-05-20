8bf35ed60c9642c08424466708229158d394a224
classicbrandco <classicbrandco@gmail.com>
2026-05-20 00:34:51 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,30 +4,19 @@
   packages = [
     pkgs.jdk17
     pkgs.gradle
-
-    # Android SDK + tools
     pkgs.android-tools
-    pkgs.androidsdk
-    pkgs.androidsdk.platform-tools
-    pkgs.androidsdk.build-tools-34-0-0
-    pkgs.androidsdk.cmdline-tools-latest
   ];
 
   env = {
-    ANDROID_HOME = "${pkgs.androidsdk}/libexec/android-sdk";
-    ANDROID_SDK_ROOT = "${pkgs.androidsdk}/libexec/android-sdk";
     JAVA_HOME = "${pkgs.jdk17}";
+    ANDROID_HOME = "/workspace/.android-sdk";
+    ANDROID_SDK_ROOT = "/workspace/.android-sdk";
   };
 
   idx = {
     extensions = [
       "google.gemini-cli-vscode-ide-companion"
     ];
-
-    previews = {
-      enable = true;
-      previews = { };
-    };
 
     workspace = {
       onCreate = {
